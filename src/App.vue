315a414bca223eefa62e7892a582e89089bcc576
nomi <67637212+kwoneunyoung@users.noreply.github.com>
2022-03-31 06:47:49 +0900

    3/31
    3/31 v-for, v-if

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,35 @@
 <template>
-  <div class="menu">
-    <a>Home</a>
-    <a>Products</a>
-    <a>About</a>
+  
+  <div class="black-bg" v-if="모달창열렸니 == true">
+    <div class="white-bg">
+      <h4>상세페이지임</h4>
+      <p>상세페이지 내용임</p>
+      <button @click="close">닫기</button>
+    </div>
   </div>
 
-  <img alt="Vue logo" src="./assets/logo.png">
+
+  <div class="menu">
+    <a v-for="item in menus" :key="item">{{ item }}</a>
+  </div>
+
   <div>
-    <h4>{{ products[0] }}</h4>
+    <img src="./assets/room0.jpg" class="room-image">
+    <h4 style="cursor:pointer;" @click="모달창열렸니 = true">{{ products[0] }}</h4>
     <p>50 만원</p>
+    <button @click="신고수[0] += 1">허위매물신고</button> <span>신고수 : {{ 신고수[0] }}</span>
   </div>
   <div>
+    <img src="./assets/room1.jpg" class="room-image">
     <h4>{{ products[1] }}</h4>
-    <p>70 만원</p>
+    <p>60 만원</p>
+    <button @click="신고수[1] += 1">허위매물신고</button> <span>신고수 : {{ 신고수[1] }}</span>
   </div>
   <div>
+    <img src="./assets/room2.jpg" class="room-image">
     <h4>{{ products[2] }}</h4>
-    <p>80 만원</p>
+    <p>70 만원</p>
+    <button @click="신고수[2] += 1">허위매물신고</button> <span>신고수 : {{ 신고수[2] }}</span>
   </div>
 </template>
 
@@ -26,9 +39,23 @@ export default {
   name: 'App',
   data() {
     return {
-      products : ['역삼동 원룸', '천호동 원룸', '마포구 원룸']
+      모달창열렸니 : false,
+      신고수 : [0,0,0],
+      menus : ['Home', 'Shop', 'About'],
+      products : ['역삼동 원룸', '천호동 원룸', '마포구 원룸'],
+      prices : ['50', '70', '80']
     }
   },
+
+  methods : {
+    increase(){
+      this.신고수 += 1;
+    },
+    close() {
+      this.모달창열렸니 = false;
+    }
+  },
+
   components: {
   }
 }
@@ -41,7 +68,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
 .menu {
@@ -53,5 +79,30 @@ export default {
 .menu a {
   color: white;
   padding: 10px;
+}
+
+.room-image {
+  width: 100%;
+  margin-top: 40px;
+}
+
+.body {
+  margin : 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%; height: 100%;
+  background: rbga(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
 }
 </style>
